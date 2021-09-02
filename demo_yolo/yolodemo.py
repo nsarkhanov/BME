7f4950a83c4d3c7a1375732a0cffe33d744ea22b
nsarkhanov <nsarkhanov@gmail.com>
2021-09-03 00:31:09 +0200

    update

--- demo_yolo/yolodemo.py
+++ demo_yolo/yolodemo.py
@@ -4,14 +4,17 @@ import numpy as np
 
 cap=cv2.VideoCapture(0)
 
+classesFile='coco.names'
+classNames=[]
+with open (classesFile,'rt') as f:
+    classNames=f.read().rstrip('\n').split('\n')
+print(classNames)
+# while True:
+#     success , img =cap.read()
+#     cv2.imshow('Image',img)
 
 
-while True:
-    success , img =cap.read()
-    cv2.imshow('Image',img)
 
 
 
-
-    
-    cv2.waitKey(1)
+#     cv2.waitKey(1)

2cd46662dd75fdb23f75ae59238774a215a613e8
nsarkhanov <nsarkhanov@gmail.com>
2021-09-07 17:09:04 +0200

    new

--- data_collector/google_image_api.py
+++ data_collector/google_image_api.py
@@ -1,4 +1,20 @@
 
+from bing_image_downloader import downloader
+classeName_file='itemNames.text'
+with open(classeName_file,'rt') as f:
+	classeNames=f.read().rstrip('\n').split('\n')
+	f.close()
+len(classeNames)
+print(classeNames)
+# downloader.download('cat', limit=10,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)
+
+
+
+
+
+
+
+
 # from bing_images import bing
 #
 # urls = bing.fetch_image_urls("cat", limit=10, file_type='png', filters='+filterui:aspect-square+filterui:color2-bw')
@@ -7,5 +23,3 @@
 # for url in urls:
 #     print("{}: {}".format(counter, url))
 #     counter += 1
-from bing_image_downloader import downloader
-downloader.download('cat', limit=10,  output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60, verbose=True)

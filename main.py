36fb48e528d69d2f92bb878c92c07abefde49d18
nsarkhanov <nsarkhanov@gmail.com>
2021-09-14 19:38:06 +0200

    update

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ import model
 import speach_model
 import time 
 text0='Hello , My name is BeME. I will assit you during my life time. First I want to know your name. Please  touch the device one time then say your name'
-text1="nice to meet you "
+text1="nice to meet you Nurlan"
 text2="I will call you friend"
 text3="when you need my help, just touch device."
 text_process="my friend ,  now I will explain my working principles  to understand me.when you  touch device i take  photo with camera of device then detect object on photo fro you."
 text4="I am happy with you"
 res=model.object_detector()
-speach_model.speak(text0)
-time.sleep(1)
-time.sleep(1)
-speach_model.speak(text1)
-time.sleep(1)
-speach_model.speak(text2)
-time.sleep(1)
-speach_model.speak(text_process)
+
+def setup_function():
+    speach_model.speak(text0)
+    time.sleep(1)
+    time.sleep(1)
+    speach_model.speak(text1)
+    time.sleep(1)
+    speach_model.speak(text2)
+    time.sleep(1)
+    speach_model.speak(text_process)
 print(res)
-time.sleep(3)
+time.sleep(1)
+
+
+setup_function()
+
 def RunApp():
     count=len(res)
     if count>1:
@@ -39,4 +45,3 @@ def RunApp():
     else:
          speach_model.speak('Sorry ,my friend,I cant detect anything')
 
-RunApp()
